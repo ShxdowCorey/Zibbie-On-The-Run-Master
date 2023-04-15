4252f7c3b97fa55f834e790d7560622dee975dc1
Corey McDonald <105554869+ShxdowCorey@users.noreply.github.com>
2023-04-15 13:06:46 -0500

    Add files via upload

---
+++ assets/data/desert_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="desert_tiles" tilewidth="16" tileheight="16" tilecount="54" columns="9">
+ <image source="../images/desert_tiles.png" width="144" height="96"/>
+</tileset>
---
+++ assets/data/misc.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="misc" tilewidth="16" tileheight="16" tilecount="5" columns="5">
+ <image source="../images/flower_assets.png" width="80" height="16"/>
+</tileset>
